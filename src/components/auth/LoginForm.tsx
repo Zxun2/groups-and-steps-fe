68013c4c0d563b2dc04499d552f5265956ab4bcd
Zong Xun <zxun2000@gmail.com>
2022-01-23 18:17:34 +0800

    Feat: Added DnD

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { Typography, Paper } from "@material-ui/core";
 import { loginStyles } from "../../styles/Style";
-import { FormEventHandler, Fragment } from "react";
+import { FormEventHandler } from "react";
 import LoginInputs from "./LoginInputs";
 import LoginButtons from "./LoginButtons";
 
@@ -17,27 +17,25 @@ const LoginForm: React.FC<LoginFormProps> = (props) => {
   const classes = loginStyles();
 
   return (
-    <Fragment>
-      <Paper
-        elevation={10}
-        component="form"
-        onSubmit={props.handleSubmit}
-        className={classes.login__form}
-      >
-        <Typography variant="h1" className={classes.linearWipe}>
-          Groups and Steps - A knowledge repository 📝
-        </Typography>
+    <Paper
+      elevation={10}
+      component="form"
+      onSubmit={props.handleSubmit}
+      className={classes.login__form}
+    >
+      <Typography variant="h1" className={classes.linearWipe}>
+        Groups and Steps - A knowledge repository 📝
+      </Typography>
 
-        <LoginInputs form={props.form} isRegistering={props.isRegistering} />
+      <LoginInputs form={props.form} isRegistering={props.isRegistering} />
 
-        <LoginButtons
-          isRegistering={props.isRegistering}
-          form={props.form}
-          loading={props.loading}
-          setIsRegistering={props.setIsRegistering}
-        />
-      </Paper>
-    </Fragment>
+      <LoginButtons
+        isRegistering={props.isRegistering}
+        form={props.form}
+        loading={props.loading}
+        setIsRegistering={props.setIsRegistering}
+      />
+    </Paper>
   );
 };
 
